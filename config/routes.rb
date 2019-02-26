c83878aaba46a99cc55809eb9b55301595dc9170
Kate Buxton <k8hunter@hotmail.com>
2019-02-26 10:42:14 +0000

    GoogleMaps API added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'staff', to: 'pages#staff', as: 'staff'
+  get 'emergencies', to: 'pages#emergencies', as: 'emergencies'
+  get 'passports', to: 'pages#passports', as: 'passports'
+  get 'contact', to: 'pages#contact', as: 'contact'
+  get 'links', to: 'pages#links', as: 'links'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
