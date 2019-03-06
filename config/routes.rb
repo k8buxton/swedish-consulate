372841081c0520605ec5410c201db3118b01b753
Kate Buxton <k8hunter@hotmail.com>
2019-03-06 11:29:57 +0000

    swedish pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   get 'passports', to: 'pages#passports', as: 'passports'
   get 'contact', to: 'pages#contact', as: 'contact'
   get 'links', to: 'pages#links', as: 'links'
+  get 'startsida', to: 'pages#startsida', as: 'startsida'
+  get 'personal', to: 'pages#personal', as: 'personal'
+  get 'nodsituationer', to: 'pages#nodsituationer', as: 'nodsituationer'
+  get 'pass', to: 'pages#pass', as: 'pass'
+  get 'kontakta', to: 'pages#kontakta', as: 'kontakta'
+  get 'lankar', to: 'pages#lankar', as: 'lankar'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
