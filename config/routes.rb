87146f0bc56497bca80d06651c714bd6ab30e4df
Kate Buxton <k8hunter@hotmail.com>
2019-04-08 16:07:43 +0100

    redirect to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   get 'pass', to: 'pages#pass', as: 'pass'
   get 'kontakta', to: 'pages#kontakta', as: 'kontakta'
   get 'lankar', to: 'pages#lankar', as: 'lankar'
+  get '*path' => redirect('/')
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
